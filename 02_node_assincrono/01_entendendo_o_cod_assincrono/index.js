1209f05709955e4d61cff1f8b5300f6d90fe0944
junior santos <sdjuniorsantos339@gmail.com>
2024-08-13 08:56:06 -0300

    node_05

---
+++ 02_node_assincrono/01_entendendo_o_cod_assincrono/index.js
@@ -0,0 +1,12 @@
+console.log("primeira linha")
+console.log("segunda linha")
+console.log("terceira linha")
+// esse e um padrão de um codigo que é direto ao ponto 
+//esse codigo e executado  sequencialmente
+
+
+setTimeout(() => {
+    console.log("executando...")
+}, 3000);
+
+console.log("quarta linha")//aqui executa antes do codigo acima
