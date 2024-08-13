6fdc954e84d7a7d9fd616031fb153674f460ed3b
junior santos <sdjuniorsantos339@gmail.com>
2024-08-13 10:33:32 -0300

    node_05

---
+++ 02_node_assincrono/02_call/index.js
@@ -0,0 +1,11 @@
+console.log("primeira parte")
+
+setTimeout(() => {
+    console.log("espera de 3 segundos")
+}, 3000);
+
+setTimeout(()=>{
+     console.log("espera de 0 segundos")
+},0);
+
+console.log("finalizando")
