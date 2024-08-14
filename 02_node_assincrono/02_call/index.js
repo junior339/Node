617dc8897285c05db7d3bc622816cd239d1905b9
junior santos <sdjuniorsantos339@gmail.com>
2024-08-14 14:19:11 -0300

    node_0.6

--- 02_node_assincrono/02_call/index.js
+++ 02_node_assincrono/02_call/index.js
@@ -9,3 +9,22 @@ setTimeout(()=>{
 },0);
 
 console.log("finalizando")
+
+//como podemos notar aqui temos um tipo de codigo assincrono 
+/*
+  vou explicar abaixo
+  primeiro o codigo executara a parte não assincrona
+  então será 
+  primeira parte 
+  e depois finalizando 
+  ae o codigo ira para a parte assincrona que é o timeout 
+  na area do node api 
+  ele vai pegar o codigo assin
+  e vai me servir a terefa que terminar antes 
+  o timeout 0
+  e depois o timeout 3 
+
+  note que mesmo ele sendo um timeout de 0 ele executa 
+  depois que o codigo normal terminar 
+
+*/
